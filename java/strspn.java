4fc74c82ac7558491ff94aff15ae7e1cfec9f513
Nathan Fiedler <nlfiedler@yahoo.com>
2008-08-15 20:22:30 +0000

    Adding strspn implementation in Java.

---
+++ java/strspn.java
@@ -0,0 +1,59 @@
+/*
+ * $Id: strspn.java 23 2007-06-22 18:40:28Z nf151384 $
+ */
+
+/**
+ * Finds the length of the string prefix that matches the 'accept'
+ * character set.
+ *
+ * Cavaets: does not take character collation into account.
+ * Runtime: O(n*m) ?
+ *
+ * @author  Nathan Fiedler
+ */
+public class strspn {
+
+    public static int accept(String s, String accept) {
+        boolean failed = false;
+        int ii = 0;
+        while (!failed && ii < s.length()) {
+            int match = 0;
+            for (int jj = 0; jj < accept.length(); jj++) {
+                if (s.charAt(ii) == accept.charAt(jj)) {
+                    match++;
+                }
+            }
+            if (match == 0) {
+                break;
+            }
+            ii++;
+        }
+        return ii;
+    }
+
+    public static void main(String[] args) {
+        String tests[] = {
+            "this is a string",
+            "hist ",
+            "string",
+            "str",
+            "string",
+            "rts",
+            "string",
+            "rst",
+            "gnirts",
+            "rst",
+        };
+        for (int ii = 0; ii < tests.length; ii += 2) {
+            int p = accept(tests[ii], tests[ii + 1]);
+            if (p == 0) {
+                System.out.println("No matches for " + tests[ii] + " and " +
+                                  tests[ii + 1]);
+            } else {
+                String s = tests[ii].substring(0, p);
+                System.out.println("'" + s + "' contains " + tests[ii + 1] +
+                                  " (" + p + " characters)");
+            }
+        }
+    }
+}
