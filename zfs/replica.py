3f641532595d61da39676bf9b8563df703936f03
Nathan Fiedler <nlfiedler@yahoo.com>
2014-06-01 18:04:12 -0700

    Greatly improved replica.py script
    * Streamlined the code substantially
    * Passes pylint and pep8 linters
    * Handle errors a little better
    * Ensure the auto-snapshot setting is restored

--- zfs/replica.py
+++ zfs/replica.py
@@ -1,85 +1,82 @@
 #!/usr/bin/env python3
+"""Script to replicate one filesystem to another in a repeatable fashion.
+
+Note that this script uses the -F option for 'zfs recv' such that the
+destination file system is rolled back before receiving the snapshot(s).
+This is necessary since otherwise the receive will fail due to the
+mismatch in existing snapshots. This occurs because simply listing a
+directory in the destination will modify the access times, which causes a
+write to the file system. The alternative is to make the destination
+read-only, but that is an extra step which can be easily avoided.
+
+"""
 #
-# Script to replicate one filesystem to another in a repeatable fashion. Note
-# that this script uses the -F option for 'zfs recv' such that the destination
-# file system is rolled back before receiving the snapshot(s). This is
-# necessary since otherwise the receive will fail due to the mismatch in
-# existing snapshots. This occurs because simply listing a directory in the
-# destination will modify the access times, which causes a write to the file
-# system. The alternative is to make the destination read-only, but that is an
-# extra step which can be easily avoided.
-#
-# To test this script, create two throw-away ZFS filesystems using the mkfile
-# command, as shown below:
+# To test this script, create two throw-away ZFS filesystems using the
+# mkfile command, as shown below:
 #
 # [root@solaris]$ mkfile 100m master
 # [root@solaris]$ mkfile 100m slave
-# [root@solaris]$ zpool create master $PWD/master
-# [root@solaris]$ zpool create slave $PWD/slave
+# [root@solaris]$ pfexec zpool create master $PWD/master
+# [root@solaris]$ pfexec zpool create slave $PWD/slave
 #
 
+import argparse
 from datetime import datetime
-import errno
-import getopt
+import os
 import re
 import subprocess
 import sys
 
-verbose = False
-debug = False
+VERBOSE = False
+DEBUG = False
 
 
-def disableauto(fs):
+def _call_proc(cmd):
+    """Invoke the command in a subprocess and return its output.
+
+    :param cmd: list of strings to pass to subprocess.call().
+
+    Raises CalledProcessError if the process exit code is non-zero.
+
+    """
+    if cmd is None or not isinstance(cmd, list):
+        raise RuntimeError("cmd must be a non-empty list")
+    if VERBOSE:
+        print(" ".join(cmd))
+    output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+    output = output.decode()
+    if DEBUG:
+        for line in output.splitlines():
+            print("=> {}".format(line))
+    return output
+
+
+def _disable_auto(fsys):
     """
     Disables the auto-snapshot service for the given file system,
     returning the previous setting (true or false).
     """
     # get the previous setting for the property
-    if verbose:
-        print("zfs get -Ho value com.sun:auto-snapshot {}".format(fs))
-    zfs = subprocess.Popen(["zfs", "get", "-Ho", "value",
-                            "com.sun:auto-snapshot", fs],
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output = zfs.communicate()[0]
-    if zfs.returncode != 0:
-        raise OSError(errno.EIO, "zfs get returned {}".format(zfs.returncode))
+    output = _call_proc(["zfs", "get", "-Ho", "value", "com.sun:auto-snapshot", fsys])
     # set the auto-snapshot property to false
-    try:
-        if verbose:
-            print("zfs set com.sun:auto-snapshot=false {}".format(fs))
-        retcode = subprocess.call(["zfs", "set",
-                                   "com.sun:auto-snapshot=false", fs])
-        if retcode < 0:
-            print("Child was terminated by signal {}".format(-retcode), file=sys.stderr)
-            sys.exit(retcode)
-    except OSError as e:
-        print("Execution failed:", e, file=sys.stderr)
-        sys.exit(1)
+    _call_proc(["zfs", "set", "com.sun:auto-snapshot=false", fsys])
     # return the previous setting
-    return output
+    return output.strip()
 
 
-def restoreauto(fs, saved):
+def _restore_auto(fsys, saved):
     """
     Restores the auto-snapshot property to the previously set value for
     the given file system.
     """
-    try:
-        if verbose:
-            print("zfs set com.sun:auto-snapshot={} {}".format(saved, fs))
-        retcode = subprocess.call(["zfs", "set", "com.sun:auto-snapshot={}".
-                                   format(saved), fs])
-        if retcode < 0:
-            print("Child was terminated by signal", -retcode, file=sys.stderr)
-            sys.exit(retcode)
-    except OSError as e:
-        print("Execution failed:", e, file=sys.stderr)
-        sys.exit(1)
+    # zfs get returns '-' when the property is not set.
+    if saved != "-":
+        _call_proc(["zfs", "set", "com.sun:auto-snapshot={}".format(saved), fsys])
 
 
-def mksnapshot(fs):
+def _take_snapshot(fsys):
     """
-    Creates a snapshot for fs whose name is today's date and time in the
+    Creates a snapshot for fsys whose name is today's date and time in the
     following format: %Y-%m-%d-%H:%M, and returns that name. The time is
     in UTC.
     """
@@ -87,55 +84,36 @@ def mksnapshot(fs):
     # as the name
     today = datetime.utcnow()
     tag = today.strftime("%Y-%m-%d-%H:%M")
-    try:
-        if verbose:
-            print("zfs snapshot {}@replica:{}".format(fs, tag))
-        retcode = subprocess.call(["zfs", "snapshot",
-                                   "{}@replica:{}".format(fs, tag)])
-        if retcode < 0:
-            print("Child was terminated by signal", -retcode, file=sys.stderr)
-            sys.exit(retcode)
-    except OSError as e:
-        print("Execution failed:", e, file=sys.stderr)
-        sys.exit(1)
+    _call_proc(["zfs", "snapshot", "{}@replica:{}".format(fsys, tag)])
     return tag
 
 
-def snapshots(fs):
+def _our_snapshots(fsys):
     """
     Get our mananged snapshots for the given file system, such that they
     are named "replica:" followed by a date in the ISO 8601 format (i.e.
     YYYY-mm-dd-HH:MM).
     """
-    if verbose:
-        print("zfs list -t snapshot -Hr {}".format(fs))
-    zfs = subprocess.Popen(["zfs", "list", "-t", "snapshot", "-Hr", fs],
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # Ignore the stderr output and only read the stdout output.
-    output = zfs.communicate()[0]
-    if zfs.returncode != 0:
-        raise OSError(errno.EIO, "zfs list returned {}".format(zfs.returncode))
-    if isinstance(output, bytes):
-        output = output.decode('utf-8')
+    output = _call_proc(["zfs", "list", "-t", "snapshot", "-Hr", fsys])
     snaps = output.splitlines()
-    prog = re.compile("@replica:\d{4}-\d{2}-\d{2}-\d{2}:\d{2}")
+    prog = re.compile(r"@replica:\d{4}-\d{2}-\d{2}-\d{2}:\d{2}")
     snaps = [snap for snap in snaps if prog.search(snap)]
     snaps = [snap.split('\t')[0] for snap in snaps]
     snaps = [snap.split('@')[1] for snap in snaps]
     snaps.sort()
-    if debug:
-        print("Existing snapshots on {}...".format(fs))
+    if DEBUG:
+        print("Existing snapshots on {}...".format(fsys))
         for snap in snaps:
             print(snap)
     return snaps
 
 
-def sendsnapshot(src, dst, tag):
+def _send_snapshot(src, dst, tag):
     """
     Send a replication stream for a single snapshot from the source
     filesystem to the destination.
     """
-    if verbose:
+    if VERBOSE:
         print("zfs send -R {}@{} | zfs recv -F {}".format(src, tag, dst))
     send = subprocess.Popen(["zfs", "send", "-R", "{}@{}".format(src, tag)],
                             stdout=subprocess.PIPE)
@@ -146,21 +124,18 @@ def sendsnapshot(src, dst, tag):
     # Read the outputs so the process finishes, but ignore them.
     recv.communicate()
     if send.returncode != 0 and send.returncode is not None:
-        raise OSError(errno.EIO, "zfs send returned {}".
-                      format(send.returncode))
-    if recv.returncode != 0 and send.returncode is not None:
-        raise OSError(errno.EIO, "zfs recv returned {}".
-                      format(recv.returncode))
+        raise subprocess.CalledProcessError(send.returncode, "zfs send")
+    if recv.returncode != 0 and recv.returncode is not None:
+        raise subprocess.CalledProcessError(recv.returncode, "zfs recv")
 
 
-def sendincremental(src, dst, tag1, tag2):
+def _send_incremental(src, dst, tag1, tag2):
     """
     Send an incremental replication stream from the source filesystem to
     the destination that spans the two snapshots.
     """
-    if verbose:
-        print("zfs send -R -I {} {}@{} | zfs recv -F {}".format(
-            tag1, src, tag2, dst))
+    if VERBOSE:
+        print("zfs send -R -I {} {}@{} | zfs recv -F {}".format(tag1, src, tag2, dst))
     send = subprocess.Popen(["zfs", "send", "-R", "-I", tag1, "{}@{}".format(src, tag2)],
                             stdout=subprocess.PIPE)
     recv = subprocess.Popen(["zfs", "recv", "-F", dst], stdin=send.stdout,
@@ -170,115 +145,109 @@ def sendincremental(src, dst, tag1, tag2):
     # Read the outputs so the process finishes, but ignore them.
     recv.communicate()
     if send.returncode != 0 and send.returncode is not None:
-        raise OSError(errno.EIO, "zfs send returned {}".
-                      format(send.returncode))
-    if recv.returncode != 0 and send.returncode is not None:
-        raise OSError(errno.EIO, "zfs recv returned {}".
-                      format(recv.returncode))
+        raise subprocess.CalledProcessError(send.returncode, "zfs send")
+    if recv.returncode != 0 and recv.returncode is not None:
+        raise subprocess.CalledProcessError(recv.returncode, "zfs recv")
 
 
-def destroysnap(fs, snap):
+def _destroy_snapshot(fsys, snap):
     """
     Destroy the named snapshot in the given file system.
     """
-    if verbose:
-        print("zfs destroy {}@{}".format(fs, snap))
-    zfs = subprocess.Popen(["zfs", "destroy", "{}@{}".format(fs, snap)],
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # Read the outputs so the process finishes, but ignore them.
-    zfs.communicate()
-    if zfs.returncode != 0:
-        raise OSError(errno.EIO, "zfs destroy returned {}".
-                      format(zfs.returncode))
+    output = _call_proc(["zfs", "destroy", "{}@{}".format(fsys, snap)])
+    if DEBUG:
+        print(output)
+
+
+def _create_and_send_snapshot(src, dst):
+    """Create a snapshot and send it to the destination.
+
+    :param src: source file system
+    :param dst: destination file system
+
+    """
+    # make the new snapshot, get a list of existing snapshots,
+    # and decide whether to send a full stream or an incremental
+    _take_snapshot(src)
+    snaps = _our_snapshots(src)
+    if snaps is None or len(snaps) == 0:
+        raise OSError("Failed to create new snapshot in {}".format(src))
+    dstsnaps = _our_snapshots(dst)
+    if dstsnaps is not None and len(dstsnaps) > 0 \
+            and dstsnaps[-1] not in snaps:
+        raise OSError("Destination snapshots out of sync with source, destroy and try again.")
+    if len(snaps) == 1:
+        # send the initial snapshot
+        _send_snapshot(src, dst, snaps[0])
+    elif dstsnaps is None or len(dstsnaps) == 0:
+        # send the latest snapshot since the destination has none
+        _send_snapshot(src, dst, snaps[-1])
+    else:
+        # destination has matching snapshots, send an incremental
+        recent = snaps[-2:]
+        _send_incremental(src, dst, recent[0], recent[1])
+
+
+def _prune_old_snapshots(src, dst):
+    """Prune the old replica snapshots from source and destination.
+
+    :param src: source file system
+    :param dst: destination file system
+
+    """
+    # prune old snapshots in source file system
+    snaps = _our_snapshots(src)
+    oldsnaps = snaps[:-2]
+    for snap in oldsnaps:
+        _destroy_snapshot(src, snap)
+    # prune old snapshots in destination file system
+    dstsnaps = _our_snapshots(dst)
+    if dstsnaps is None or len(dstsnaps) == 0:
+        raise OSError("Failed to create new snapshot in {}".format(dst))
+    oldsnaps = dstsnaps[:-2]
+    for snap in oldsnaps:
+        _destroy_snapshot(dst, snap)
 
 
 def main():
-    # parse the command line arguments
-    shortopts = "h"
-    longopts = ["help"]
-    # keep using getopt until Python 2.7 is available on OpenIndiana,
-    # then we can switch to using argparse
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], shortopts, longopts)
-    except getopt.GetoptError as err:
-        print(str(err))
-        print("Invoke with -h for help.")
-        sys.exit(2)
-    for opt, val in opts:
-        if opt in ("-h", "--help"):
-            usage()
-            sys.exit()
-        else:
-            assert False, "unhandled option: {}".format(opt)
-
-    if len(args) < 2:
-        print("Usage: replica.py [options] <srcfs> <dstfs>")
-        print("Invoke with --help for helpful information")
-        sys.exit(0)
-
-    src = args[0]
-    dst = args[1]
-
-    try:
-        # disable the auto-snapshot service to prevent spurious failures
-        assrc = disableauto(src)
-        asdst = disableauto(dst)
-        # make the new snapshot, get a list of existing snapshots,
-        # and decide whether to send a full stream or an incremental
-        mksnapshot(src)
-        snaps = snapshots(src)
-        if snaps is None or len(snaps) == 0:
-            print("Failed to create new snapshot in {}".format(src))
-            sys.exit(1)
-        dstsnaps = snapshots(dst)
-        if dstsnaps is not None and len(dstsnaps) > 0 \
-                and dstsnaps[-1] not in snaps:
-            print("Destination snapshots out of sync with source, destroy and try again.")
-            sys.exit(1)
-        if len(snaps) == 1:
-            # send the initial snapshot
-            sendsnapshot(src, dst, snaps[0])
-        elif dstsnaps is None or len(dstsnaps) == 0:
-            # send the latest snapshot since the destination has none
-            sendsnapshot(src, dst, snaps[-1])
-        else:
-            # destination has matching snapshots, send an incremental
-            recent = snaps[-2:]
-            sendincremental(src, dst, recent[0], recent[1])
-
-        # prune old snapshots in source file system
-        oldsnaps = snaps[:-2]
-        for snap in oldsnaps:
-            destroysnap(src, snap)
-        # prune old snapshots in destination file system
-        dstsnaps = snapshots(dst)
-        if dstsnaps is None or len(dstsnaps) == 0:
-            print("Failed to create new snapshot in {}".format(dst))
-            sys.exit(1)
-        oldsnaps = dstsnaps[:-2]
-        for snap in oldsnaps:
-            destroysnap(dst, snap)
-        # restore the auto-snapshot property on the file systems
-        restoreauto(dst, asdst)
-        restoreauto(src, assrc)
-    except OSError as e:
-        print("Execution failed:", e, file=sys.stderr)
-        sys.exit(1)
-
-
-def usage():
-    print("""Usage: replica.py [-h] <srcfs> <dstfs>
-
-This script creates a snapshot on the source ZFS file system and sends
+    """Do the work."""
+    desc = """This script creates a snapshot on the source ZFS file system and sends
 that in the form of a replication stream to the destination file system.
 If a previous snapshot created by this script exists then this script
 will create a new snapshot and send an incremental replication stream to
 the destination. Older snapshots on both the source and destination will
-be automatically pruned such that the two most recent are retained.
+be automatically pruned such that the two most recent are retained."""
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('src', help='Source ZFS file system')
+    parser.add_argument('dest', help='Destination ZFS file system')
+    args = parser.parse_args()
+    src = args.src
+    dst = args.dest
 
--h|--help
-\tPrints this usage information.
-""")
+    # disable the auto-snapshot service to prevent spurious failures
+    try:
+        assrc = _disable_auto(src)
+        asdst = _disable_auto(dst)
+    except subprocess.CalledProcessError as cpe:
+        print("Unable to acquire/modify auto-snapshot status.", file=sys.stderr)
+        if cpe.output:
+            sys.stderr.write(cpe.output.decode())
+        sys.exit(cpe.returncode)
+    try:
+        _create_and_send_snapshot(src, dst)
+        _prune_old_snapshots(src, dst)
+    except subprocess.CalledProcessError as cpe:
+        if cpe.output:
+            sys.stderr.write(cpe.output.decode())
+        sys.exit(cpe.returncode)
+    except OSError as ose:
+        sys.stderr.write(str(ose))
+        sys.exit(os.EX_OSERR)
+    finally:
+        # restore the auto-snapshot property on the file systems
+        _restore_auto(dst, asdst)
+        _restore_auto(src, assrc)
+
 
 if __name__ == "__main__":
     main()
