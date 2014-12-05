511a6a0ab8cb21e89eccfa0de0126628f46e258d
Nathan Fiedler <nathanfiedler@fastmail.fm>
2014-12-04 16:33:22 -0800

    Count files as well as lines

--- linecount.py
+++ linecount.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
-#
-# Counts lines of code in Python scripts. Bascially a fixed version
-# of http://code.activestate.com/recipes/527746-line-of-code-counter/
-#
+"""Counts lines of code in Python scripts.
+
+Bascially a fixed version
+of http://code.activestate.com/recipes/527746-line-of-code-counter/
+
+"""
 
 import os
 import fnmatch
 
 
 def walk(root='.', recurse=True, pattern='*'):
-    """Generator for walking a directory tree. Starts at specified root
-    folder, returning files that match our pattern. Optionally will also
-    recurse through directories.
+    """Generator for walking a directory tree.
+
+    Starts at specified root folder, returning files that match our
+    pattern. Optionally will also recurse through directories.
+
     """
     for path, subdirs, files in os.walk(root):
         for name in files:
@@ -22,17 +26,20 @@ def walk(root='.', recurse=True, pattern='*'):
 
 
 def count(root='.', recurse=True):
-    """Counts lines of code in two ways:
-    * maximal size (source LOC) with blank lines and comments
-    * minimal size (logical LOC) stripping same
+    """Count lines of code.
 
-    Sums all Python files in the specified folder.
-    By default recurses through subfolders.
+    Returns maximal size (source LOC) with blank lines and comments as well
+    as minimal size (logical LOC) stripping same.
+
+    Sums all Python files in the specified folder. By default recurses
+    through subfolders.
+
     """
-    count_mini, count_maxi = 0, 0
+    files, count_mini, count_maxi = 0, 0, 0
     for fspec in walk(root, recurse, '*.py'):
         skip = False
         with open(fspec) as f:
+            files += 1
             for line in f:
                 count_maxi += 1
                 line = line.strip()
@@ -44,9 +51,9 @@ def count(root='.', recurse=True):
                         continue
                     if not skip:
                         count_mini += 1
-    return count_mini, count_maxi
+    return files, count_mini, count_maxi
 
 
 if __name__ == '__main__':
-    mini, maxi = count()
-    print("Maximal lines: {}\nMinimal lines: {}".format(maxi, mini))
+    files, mini, maxi = count()
+    print("File count: {}\nMaximal lines: {}\nMinimal lines: {}".format(files, maxi, mini))
