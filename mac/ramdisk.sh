2b3c63f55703fef201e62faebfb06c011e94c44b
Nathan Fiedler <nathanfiedler@fastmail.fm>
2016-08-31 10:18:12 -0700

    Compute appropriate size for RAM disk

--- mac/ramdisk.sh
+++ mac/ramdisk.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# Script that performs several tasks:
+# Script that performs the following tasks:
 #
-# 1. Set up a 4GB RAM disk
+# 1. Set up a RAM disk of reasonable size
 # 2. Spawn child shell process with TMPDIR set to RAM disk
 # 3. Eject RAM disk when child shell exits
 #
@@ -10,14 +10,20 @@
 RNAME='RamDisk'
 RPATH="/Volumes/${RNAME}"
 
+#
+# Get the size in bytes of the available RAM on this system, then divide by
+# 8 and divide that by the sector size (512 bytes) to arrive at the number
+# of sectors to be allocated to the RAM disk.
+#
+let SECTORS=`sysctl -n hw.memsize`/8/512
+
 # Check if already mounted
 mount | grep -q $RPATH
 if [ $? == 0 ]; then
     DEV=`mount | grep $RPATH | cut -d ' ' -f 1`
     echo "Using existing RAM disk ${DEV}..."
 else
-    # Create a ~4GB RAM disk, formatted as HFS+
-    DEV=`hdiutil attach -nomount ram://8400000`
+    DEV=`hdiutil attach -nomount ram://$SECTORS`
     diskutil erasevolume "Case-sensitive HFS+" $RNAME $DEV
 fi
 
