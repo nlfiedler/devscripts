fcea707ccb5e4ca40712e45885aad96a3d1f276c
Nathan Fiedler <nfiedler@perforce.com>
2019-01-10 11:43:48 -0800

    Detect unset P4PASSWD value

--- perforce/p4login.py
+++ perforce/p4login.py
@@ -9,6 +9,9 @@ def main():
     """Log in to the Perforce server."""
     # Yep, pretty much that easy.
     result = subprocess.check_output(['p4', 'set', '-q', 'P4PASSWD'])
+    if '=' not in result:
+        sys.stderr.write('Missing P4PASSWD setting!\n')
+        sys.exit(2)
     passwd = result.strip().split('=')[1]
     proc = subprocess.Popen(['p4', 'login'], stdin=subprocess.PIPE)
     proc.communicate(passwd)
