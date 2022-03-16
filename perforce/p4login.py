ac9bb84502b2e32f3f920a3ce9e244a200d74600
Nathan Fiedler <nfiedler@perforce.com>
2022-03-16 10:50:27 -0700

    fix: migrate to python3 API usage

--- perforce/p4login.py
+++ perforce/p4login.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Script to automate logging into Perforce."""
 
 import subprocess
@@ -8,12 +8,12 @@ import sys
 def main():
     """Log in to the Perforce server."""
     # Yep, pretty much that easy.
-    result = subprocess.check_output(['p4', 'set', '-q', 'P4PASSWD'])
+    result = subprocess.check_output(['p4', 'set', '-q', 'P4PASSWD'], text=True)
     if '=' not in result:
         sys.stderr.write('Missing P4PASSWD setting!\n')
         sys.exit(2)
     passwd = result.strip().split('=')[1]
-    proc = subprocess.Popen(['p4', 'login'], stdin=subprocess.PIPE)
+    proc = subprocess.Popen(['p4', 'login'], stdin=subprocess.PIPE, text=True)
     proc.communicate(passwd)
     sys.exit(proc.returncode)
 
