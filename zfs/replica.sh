6ace25283b5357d05397a0bb8c527f819ace372a
Nathan Fiedler <nathanfiedler@fastmail.fm>
2025-10-04 10:01:48 -0700

    Add debugging to ZFS replication script

--- zfs/replica.sh
+++ zfs/replica.sh
@@ -4,14 +4,22 @@
 # and replication streams (zfs send | zfs recv). Can send the data set to a
 # remote host using SSH.
 #
-# To test this script, create two throw-away ZFS filesystems, where "tank" is
-# the name of the pool in which the file systems will be created:
+# Preparation
 #
-# $ sudo zfs create tank/source
+# $ ssh-copy-id -i ~/.ssh/id_rsa hostaddr
+# $ sudo su -
+# # ssh-copy-id -i .ssh/id_rsa.pub username@hostaddr
 #
-# $ sudo zfs create tank/target
+# Troubleshooting
+#
+# "target zfs dataset <dataset> is not present"
+#    The SSH connection is not set up correctly, see above.
+#
+# "<username>@<hostaddr>'s password:"
+#    The SSH connection is not set up correctly, see above.
 #
 MONOCHROME=false
+DEBUG=false
 INCREMENTAL=false
 SOURCE_NAME=''
 TARGET_NAME=''
@@ -28,6 +36,13 @@ function error() {
 function die() {
     error "FATAL: $*" >&2
     exit 1
+}
+
+# Print the first argument in blue text on STDERR.
+function debug() {
+    $DEBUG || return 0
+    $MONOCHROME || echo -e "\033[33m$1\033[0m" >&2
+    $MONOCHROME && echo -e "$1" >&2 || true
 }
 
 function usage() {
@@ -86,6 +101,9 @@ function read_arguments() {
             usage
             exit 0
             ;;
+        --debug)
+            DEBUG=true
+            ;;
         -m)
             MONOCHROME=true
             ;;
@@ -129,6 +147,7 @@ function ensure_readiness() {
 
 # Ensure the named source dataset is actually present.
 function ensure_source() {
+    debug 'checking source dataset exists'
     # test by retrieving a setting that all ZFS datasets have
     zfs get compression ${SOURCE_NAME} >/dev/null 2>&1
     if (( $? != 0 )); then
@@ -138,12 +157,13 @@ function ensure_source() {
 
 # Ensure the named target dataset is actually present.
 function ensure_target() {
+    debug 'checking target dataset exists'
     # test by retrieving a setting that all ZFS datasets have
     local CMD="zfs get compression ${TARGET_NAME} >/dev/null 2>&1"
     if [[ -n "${HOST_NAME}" ]]; then
         CMD="ssh ${HOST_NAME} ${CMD}"
     fi
-    ${CMD}
+    eval "${CMD}"
     if (( $? != 0 )); then
         die "target zfs dataset ${TARGET_NAME} is not present"
     fi
@@ -151,6 +171,7 @@ function ensure_target() {
 
 # Take a snapshot on the source with the name "replica:<datetime>"
 function take_snapshot() {
+    debug 'taking snapshot'
     local DATE_NOW=$(date +'%Y-%m-%d-%T')
     local TAG_NAME="replica:${DATE_NOW}"
     zfs snapshot "${SOURCE_NAME}@${TAG_NAME}"
@@ -165,12 +186,14 @@ function get_snapshot_tags() {
 
 # Output a list of the source snapshots created by this script.
 function our_source_snapshots() {
+    debug 'gathering source snapshot list'
     local OUTPUT=$(zfs list -H -o name -r -t snapshot ${SOURCE_NAME})
     get_snapshot_tags "$OUTPUT"
 }
 
 # Output a list of the target snapshots created by this script.
 function our_target_snapshots() {
+    debug 'gathering target snapshot list'
     local CMD="zfs list -H -o name -r -t snapshot ${TARGET_NAME}"
     if [[ -n "${HOST_NAME}" ]]; then
         CMD="ssh ${HOST_NAME} ${CMD}"
@@ -192,16 +215,19 @@ function build_recv_cmd() {
 
 # Send a full replication stream for the named snapshot.
 function send_first_snapshot() {
+    debug 'sending initial snapshot'
     zfs send -R "${SOURCE_NAME}@$1" | $(build_recv_cmd)
 }
 
 # Send an incremental replication stream for the named snapshots.
 function send_incremental() {
+    debug 'sending incremental snapshot'
     zfs send -R -I $1 "${SOURCE_NAME}@$2" | $(build_recv_cmd)
 }
 
 # Prune the old replica snapshots from source dataset.
 function prune_source_snapshots() {
+    debug 'pruning old source snapshots'
     # prune old snapshots in source file system
     if (( ${#SOURCES[*]} < 3 )); then
         return
@@ -214,6 +240,7 @@ function prune_source_snapshots() {
 
 # Prune the old replica snapshots from target dataset.
 function prune_target_snapshots() {
+    debug 'pruning old target snapshots'
     if (( ${#TARGETS[*]} < 3 )); then
         return
     fi
